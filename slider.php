84edcd2ba609fd04a849c11c7883e95d87705665
quanton81 <antonduoda@yahoo.it>
2016-03-25 13:08:04 +0100

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="description" content="Pure simple css div slider">
+        <meta name="keywords" content="pure css slider, css div slider, pure css div slider">
+        <meta name="author" content="Dott. Anton Duoda">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <title>Quanton pure CSS Div Slider</title>
+    </head>
+    <body>
+        <div class="q-slider">
+            <input name="q-an-n" id="q-sl-0" type="radio" class="q-an sl">
+            <input name="q-an-n" id="q-sl-1" type="radio" class="q-an sl">
+            <input name="q-an-n" id="q-sl-2" type="radio" class="q-an sl">
+            <input name="q-an-n" id="q-pl" type="radio" class="q-an" checked>
+            <input name="q-an-n" id="q-pa-0" type="radio" class="q-an pa">
+            <input name="q-an-n" id="q-pa-1" type="radio" class="q-an pa">
+            <input name="q-an-n" id="q-pa-2" type="radio" class="q-an pa">
+            <ul>
+                <div>
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Start" style="width: 100%;">
+                </div>
+                <li class="n-0 q-im">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=First" alt="1° Slider" title="First">
+                </li>
+                <li class="n-1 q-im">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Second" alt="2° Slider" title="Second">
+                </li>
+                <li class="n-2 q-im">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Third" alt="3° Slider" title="Third">
+                </li>
+            </ul>
+            <div class="q-de">
+                <label class="n-0">
+                    <span class="q-ti"><span class="q-wr">First</span></span>
+                    <br><span class="q-de"><span class="q-wr">1° Slider</span></span>
+                </label>
+                <label class="n-1">
+                    <span class="q-ti"><span class="q-wr">Second</span></span>
+                    <br><span class="q-de"><span class="q-wr">2° Slider</span></span>
+                </label>
+                <label class="n-2">
+                    <span class="q-ti"><span class="q-wr">Third</span></span>
+                    <br><span class="q-de"><span class="q-wr">3° Slider</span></span>
+                </label>
+            </div>
+            <div class="q-arrp">
+                <label class="n-0" for="q-sl-0"><span><i></i></span></label>
+                <label class="n-1" for="q-sl-1"><span><i></i></span></label>
+                <label class="n-2" for="q-sl-2"><span><i></i></span></label>
+            </div>
+            <div class="q-arrn">
+                <label class="n-0" for="q-sl-0"><span><i></i></span></label>
+                <label class="n-1" for="q-sl-1"><span><i></i></span></label>
+                <label class="n-2" for="q-sl-2"><span><i></i></span></label>
+            </div>
+            <div class="q-bu">
+                <label class="n-0" for="q-sl-0">
+                    <span class="q-po"></span>
+                    <span class="q-th"><img src="http://placehold.it/100x50/60C5F0/000000?text=First" alt="First" title="First"></span>
+                </label>
+                <label class="n-1" for="q-sl-1">
+                    <span class="q-po"></span>
+                    <span class="q-th"><img src="http://placehold.it/100x50/60C5F0/000000?text=Second" alt="Second" title="Second"></span>
+                </label>
+                <label class="n-2" for="q-sl-2">
+                    <span class="q-po"></span>
+                    <span class="q-th"><img src="http://placehold.it/100x50/60C5F0/000000?text=Third" alt="Third" title="Third"></span>
+                </label>
+            </div>
+            <div class="q-pl-pa">
+                <label class="play1" for="q-pl">
+                    <span class="q-po"></span>
+                </label>
+                <label class="pause0" for="q-pa-0">
+                    <span class="q-po"></span>
+                </label>
+                <label class="pause1" for="q-pa-1">
+                    <span class="q-po"></span>
+                </label>
+                <label class="pause2" for="q-pa-2">
+                    <span class="q-po"></span>
+                </label>
+            </div>
+            <div class="q-ge">
+                <div>
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Start" alt="Start" title="Start" class="q-fi-im">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=First" alt="First" title="First">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Second" alt="Second" title="Second">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=Third" alt="Third" title="Third">
+                    <img src="http://placehold.it/600x400/60C5F0/000000?text=End" alt="End" title="End" class="q-la-im">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
